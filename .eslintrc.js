ef062dbb717d9df984d4225181abfbc474e54c37
lfp1024 <lfp1024@126.com>
2020-07-20 20:27:01 +0800

    update eslintrc, add parseOptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,9 @@ module.exports = {
         "node": true
     },
     "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 8
+    },
     "rules": {
         "no-unused-vars": [1, { 'vars': 'all', 'args': 'none' }]
     }
