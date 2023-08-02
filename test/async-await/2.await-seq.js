db595ec381bd3e15aa2dc67a0f283015775e60cc
lfp1024 <lfp1024@126.com>
2023-08-02 17:20:06 +0800

    test: timer 与 microtask 执行顺序

--- test/async-await/2.await-seq.js
+++ test/async-await/2.await-seq.js
@@ -9,22 +9,22 @@ let bread = 0;
 
 // 最终任务同步执行，两个入口都添加微任务，同时访问 bread变量，导致数据不一致
 // 实际引用中，如果bread保存在数据库中，则在数据库层面解决（事务、乐观锁）
-async function setBread(who) {
-    bread += 1;
-    console.log(who, '蒸馒头', bread);
+// async function setBread(who) {
+//     bread += 1;
+//     console.log(who, '蒸馒头', bread);
 
-    console.log(`${who} 蒸馒头 调用返回`)
-}
+//     console.log(`${who} 蒸馒头 调用返回`)
+// }
 
 // 最终任务异步执行
-// async function setBread() {
-//     return await new Promise(res => {
-//         setTimeout(() => { // 宏任务，第一个 setTimeout 添加的微任务执行完毕才会执行第二个
-//             bread += 1;
-//             res();
-//         }, 0)
-//     });
-// }
+async function setBread() {
+    return await new Promise(res => {
+        setTimeout(() => { // 宏任务
+            bread += 1;
+            res();
+        }, 0)
+    });
+}
 
 function getBread(who) {
     console.log(who, '取馒头', bread);
@@ -71,6 +71,7 @@ async function cook() { //=======
     console.log('entrance 2')
 })();
 
+// setBread 同步执行 ==============================
 // 队列情况
 /**
  * 主流程
@@ -98,3 +99,37 @@ async function cook() { //=======
 // 卖   取馒头 0
 // entrance 2
 // Error: sell but no bread
+
+// setBread异步执行 =================================
+
+// node 10 ==========
+// 赶集
+// 赶集 预订馒头 0
+// 做饭
+// 做饭 预订馒头 0
+// 赶集 预定馒头 调用返回
+// 做饭 预定馒头 调用返回 // 可以看出 node10，对于 timer阶段，是全部执行完之后才执行 microtask 队列
+// 赶集 调用返回
+// 卖  取馒头 2
+// 赶集 卖馒头 剩余 1
+// 做饭 调用返回
+// 吃  取馒头 1
+// 做饭 吃馒头 剩余 0
+// entrance 1
+// entrance 2
+
+// node 18 ===========
+// 赶集
+// 赶集 预订馒头 0
+// 做饭
+// 做饭 预订馒头 0
+// 赶集 预定馒头 调用返回
+// 赶集 调用返回
+// 卖  取馒头 1
+// 赶集 卖馒头 剩余 0
+// entrance 1
+// 做饭 预定馒头 调用返回 // node >10，对于 timer阶段，每个 timer 执行完毕都会先执行 microtask 队列
+// 做饭 调用返回
+// 吃  取馒头 1
+// 做饭 吃馒头 剩余 0
+// entrance 2
